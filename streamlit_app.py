75d24e945698c09a8f8c33c3b9b0d3e82f1083e8
Sushil K Das <121654872+sushildas1@users.noreply.github.com>
2022-12-30 23:12:11 -0600

    Update streamlit_app.py
    Formatted positioning of packages, added error handling package, added stop to not run anything past a beyond line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -9,7 +14,7 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -22,14 +27,17 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) #writes the json data in the screen
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #normalizes the above json data
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#Do not run anything past here until debugging happens
+streamlit.stop()
+
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM pc_rivery_db.public.FRUIT_LOAD_LIST")
